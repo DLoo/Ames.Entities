f5531d2cf0b6870a13cee3c2c1ec757624f12f02
donald <loo.donald@gmail.com>
2014-08-06 13:06:46 +0800

    Initial Setup

---
+++ EFileInfo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ames.Entities {
+    public class EFileInfo {
+
+        [Key]
+        public int EFileID { get; set; }
+
+        public DateTime CreatedDateTime { get; set; }
+
+        //[Range(1900,2100)]
+        public int Year { get; set; }
+
+        //[Range(1,12)]
+        public int Month { get; set; }
+
+        //[StringLength(500)]
+        public string DirectoryPath { get; set; }
+
+        //[StringLength(150)]
+        //[FileExtensions(Extensions="pdf|xls|doc|xlsx|docx|ppt|pptx")]
+        public string EFileName { get; set; }
+
+        //[StringLength(50)]
+        public string Location { get; set; }
+
+        //[StringLength(50)]
+        public string Brand { get; set; }
+
+        //[StringLength(50)]
+        public string Department { get; set; }
+
+        //[StringLength(50)]
+        public string Type { get; set; }
+
+        //[StringLength(50)]
+        public string GeneratedFrom { get; set; }
+
+        public DateTime? ExpiryDate { get; set; }
+
+        public Guid FileGUID { get; set; }
+
+
+    }
+}
---
+++ WebApiParameters.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ames.Entities {
+    public class UploadFileInfo {
+        public UploadFileInfo() {
+            ContentType = "application/octet-stream";
+        }
+        public string Name { get; set; }
+        public string FileName { get; set; }
+        public string ContentType { get; set; }
+        public byte[] ByteArray { get; set; }
+    }
+
+    public class WebApiParameters {
+        public IDictionary<string, string> Parameters { get; set; }
+        public IEnumerable<UploadFileInfo> UploadFiles { get; set; }
+
+    }
+}
